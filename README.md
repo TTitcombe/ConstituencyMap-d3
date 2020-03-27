a46de42c18e08bea94b3ebd8b039b90af52c8701
Tom Titcombe <t.j.titcombe@gmail.com>
2020-03-27 07:03:26 +0000

    Create simple map
    Circles are not alternating or fit to the svg, but they are roughly in place

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ConstituencyMap
 A map of UK parliamentary constituencies created in D3
+
+## How to run
+1. Clone this repository
+2. Navigate to the repository and run a server. This can be done by:
+  - Using a plugin on a JavaScript IDE
+  - Run `python -m http.server` in the terminal
+3. Navigate in a browser to the localhost which contains the application
+
+## TODO
+- [ ] Offset constituency circles on alternate rows
+- [ ] Fit map to SVG size
+- [ ] Combine map data with 2019 parliamentary results
+- [ ] Colour circles by party
+- [ ] Make circles interactive
--- mapper.js
+++ mapper.js
@@ -5,9 +5,6 @@ const titleText = "Constituency Map of the UK";
 const height = +svg.attr("height");
 const width = +svg.attr("width");
 
-const innerHeight = height - 100;
-const innnerWidth = width - 50;
-
 /**
  * Turn hexmap coordinates into SVG coordinates
  * @param  {array} dataRow parliamentary constituency data, containing coordinates p,q of hexmap position
@@ -24,17 +21,41 @@ hexToSVG = dataRow => {
  * @return {None}
  */
 createMap = data => {
-  // Offset the map from the topleft corner
-  const map = svg.append("g")
-                .attr("transform", "translate(50, 100)");
+    const xValue = d => d.q;
+    const yValue = d => d.r;
 
-  // Add a title to the top of the map
-  map.append("text")
-    .attr("id", "title")
-    .attr("transform", "translate(400,0)")
-    .text(titleText);
+    // Offset the map from the topleft corner
+    const map = svg.append("g")
+        .attr("transform", "translate(50, 100)");
+
+    const xScale = d3.scaleLinear()
+        .domain([d3.min(data, xValue), d3.max(data, xValue)])
+        .range([0, width - 50]);
+
+    const yScale = d3.scaleLinear()
+        .domain([d3.min(data, yValue), d3.max(data, yValue)])
+        .range([0, height - 100]);
+
+    // Add a title to the top of the map
+    map.append("text")
+        .attr("id", "title")
+        .attr("transform", "translate(100,0)")
+        .text(titleText);
+
+    // Add constituency circles
+    map.selectAll("circle").data(data)
+        .enter().append("circle")
+        .attr("id", "constituency")
+        .attr("cx", d => xScale(xValue(d)))
+        .attr("cy", d => height - 100 - yScale(yValue(d)))
+        .attr("r", 5);
+
 };
 
 d3.csv("uk_hex.csv").then(data => {
-  createMap(data);
+    data.forEach(d => {
+    d.q = +d.q;
+    d.r = +d.r;
+    });
+    createMap(data);
 });
